8d98f1e84216b8176a6e039d68ea1eddac329195
MLee21 <minnie.lee89@gmail.com>
2015-02-22 15:10:26 -0700

    added tests to make sure a head node can be created if list is empty.

--- linked_list_test.rb
+++ linked_list_test.rb
@@ -11,11 +11,11 @@ class NodeTest < MiniTest::Test
   end
 
   def setup
-    @node = Node.new("head_node", nil)
+    @node = Node.new(nil, nil)
   end
 
   def test_a_node_is_created_and_has_a_pointer_to_next_node
-    assert_equal "head_node", @node.value
+    assert_equal nil, @node.value
   end
 
   def test_head_node_has_a_reference_to_next_node
@@ -30,18 +30,35 @@ end
       @linked_list = LinkedList.new
     end
 
-  def test_it_can_append_a_new_element
-    skip
-    @linked_list.append("first_link_data") 
-    assert_equal 1, @linked_list.link_list_count
-  end
+    def test_if_list_is_empty
+      @node = Node.new(nil,nil)
+      @linked_list.prepend(nil)
+      assert_equal nil, @node.value
+    end
 
-  def test_it_can_append_another_new_element
-    skip
-    @linked_list.append("next_link")
-    @linked_list.append("next_link")
-    assert_equal 2, @linked_list.link_list_count
-  end
+    def test_it_has_a_head_node
+      @node = Node.new(nil, "Gabe")
+      @linked_list.prepend("Gabe")
+      assert_equal "Gabe", @node.next_node
+    end
+
+    def test_it_will_print_out_node_and_a_pointer_to_next_node
+      skip
+      @node = Node.new(nil, "Gabe")
+      @linked_list.prepend("Gabe")
+      assert_equal "Gabe -> ", @node.to_s
+    end
+
+    def test_it_can_append_a_new_element
+     skip
+    end
+
+    def test_it_can_append_another_new_element
+      skip
+      @linked_list.append("next_link")
+      @linked_list.append("next_link")
+      assert_equal 2, @linked_list.link_list_count
+    end
 
 
 end
